70a4b42221465413a9654724b6396f6b0e842af4
shkappaKartel <sonomanik@mail.ru>
2024-04-29 20:39:32 +0500

    d

---
+++ Homeworks/Module 1/hw 5.py
@@ -0,0 +1,55 @@
+my_list = ["apple", "banana", "peach", "pineapple", "mango"]
+print("First and last element: " + my_list[0], my_list[-1])
+print("From 3 to 5: " + str(my_list[2:5]))
+my_list[2] = "coconut"
+print("Add coconut in 3: " + str(my_list))
+
+my_dict = {'aplle': 'яблоко', 'orange': 'апельсин', 'mango': 'манго'}
+print(my_dict)
+print("Apple: " + my_dict['aplle'])
+my_dict.update({'kiss': 'поцелуй'})
+print(my_dict)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# my_box = {'слон': 'elephant', 'машина': 'car', 'number': 89870235111}
+# print(my_box)
+#
+# phone_book = {'Misha': 11255448888, 'Liza': 89899944555}
+# phone_book['Archi'] = 555888444499
+# print(phone_book)
+# del phone_book['Archi']
+# print("Удаление Archi из списка")
+# print(phone_book)
+# phone_book.update({'Sasha': 55544433322,
+#                    'Alex': 6565658899})
+# print("Добавление Sasha и Alex")
+# print(phone_book)
+# print("Номер Alex: " + str(phone_book.get('Alex')))
+#
+# b = phone_book.pop('Liza')
+# print(b)
+# print(phone_book)
+# print(phone_book.keys())
+# print(phone_book.values())
+#
+# set_ = {1, 2, 3, 3, 1, 2}
+# print(set_)
+#
+# list_ = [1, 2, 2, 3, True, True, 3]
+# print(list_)
