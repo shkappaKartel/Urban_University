f1c5bcd70f3eb898616e7c27db3b3dab31676367
shkappaKartel <sonomanik@mail.ru>
2024-05-06 02:03:09 +0500

    def test()

--- Homeworks/module 2/hw5.py
+++ Homeworks/module 2/hw5.py
@@ -1,12 +1,14 @@
-def test():
+def test1():
     a = 5
     b = 10
     print(a, b)
 
-test()
+
+test1()
 
 
-def test(a, b,*, c):
+def test2(a, b, *, c):
     print(a, b, c)
 
-test(1, 2, c = 3)
+
+test2(1, 2, c=3)
