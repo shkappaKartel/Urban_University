d5f151544e607e5f41ecf0c10638b8ac1f7f5424
shkappaKartel <sonomanik@mail.ru>
2024-05-22 21:51:13 +0500

    outdated

--- Homeworks/module 2/module2hard.py
+++ Homeworks/module 2/module2hard.py
@@ -8,8 +8,8 @@ def generate_password(n):
 
 
 # БЫСТРАЯ ПРОВЕРКА ГЕНЕРАТОРА
-# for num in range(3, 21):
-#     print(num, "-", generate_password(num))
+for num in range(3, 21):
+    print(num, "-", generate_password(num))
 
 num = input('Введите число, которое выпадет в первой ячейке (3-20) >>> ')
 num = int(num)
@@ -17,5 +17,7 @@ num = int(num)
 if num < 3 or num > 20:
     print('Ошибка! 0_o Введите число от 1 до 20')
 
-if 3 < num < 20:
+if 3 <= num <= 20:
     print(f'[{num}] - [{generate_password(num)}]')
+
+
