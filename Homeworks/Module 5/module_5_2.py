4337f0ebbd1cbb0336d7fd86e2f9f060b659684f
shkappaKartel <sonomanik@mail.ru>
2024-06-09 12:50:32 +0500

    module 5 zadanie 1

---
+++ Homeworks/Module 5/module_5_2.py
@@ -0,0 +1,11 @@
+class House:
+    def __init__(self):
+        self.numberOfFloors = 0
+
+    def setNewNumberOfFloors(self, floors):
+        self.numberOfFloors = floors
+        print('Количество этажей: ', self.numberOfFloors)
+
+
+house = House()
+house.setNewNumberOfFloors(10)
